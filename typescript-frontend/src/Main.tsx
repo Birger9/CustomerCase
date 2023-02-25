1520c79f63dd0272ba151d230f8649ffbae9f3c6
Erik <eribi813@student.liu.se>
2023-02-25 22:26:17 +0100

    "Add all navbar pages"

--- typescript-frontend/src/Main.tsx
+++ typescript-frontend/src/Main.tsx
@@ -3,12 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 // Own files.
 import { LoginPage } from './Screens/LoginPage';
 import { InventoryPage } from './Screens/InventoryPage';
+import { ProductsPage } from './Screens/ProductsPage';
+import { WarehousesPage } from './Screens/WarehousesPage';
+import { DeliveriesPage } from './Screens/DeliveriesPage';
 
 const Main = () => {
     return (     
         <Routes>
             <Route path='/' element={<LoginPage/>} />
             <Route path='/inventory' element={<InventoryPage/>} />
+            <Route path='/products' element={<ProductsPage/>} />
+            <Route path='/warehouses' element={<WarehousesPage/>} />
+            <Route path='/deliveries' element={<DeliveriesPage/>} />
         </Routes>
     );
 }
---
+++ typescript-frontend/src/Screens/WarehousesPage.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Nav from "../Components/Nav/Nav";
+
+//import jwt_decode from "jwt-decode";
+//import { useNavigate } from "react-router-dom";
+
+export const WarehousesPage: React.FC = () => {
+    // Used for snackbar.
+    const [open, setOpen] = useState(false);
+    const [msg, setMsg] = useState("");
+
+    //const navigate = useNavigate();
+    //const handleGoToRegister = () => navigate("/register");
+    
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const openSnackBar = (msg: string) => {
+        setOpen(true);
+        setMsg(msg);
+    };
+
+    return (
+        <div>
+            <Nav />
+        </div>
+    );
+}
